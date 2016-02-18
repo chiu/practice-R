c8ffa8e5c135dac73ca938348c6952ce1802eebc
chiu <chiucode@gmail.com>
2016-02-17 16:57:54 -0800

    trying private and public methods

--- R6Class/R6ClassPractice.R
+++ R6Class/R6ClassPractice.R
@@ -22,3 +22,50 @@ Person <- R6Class(
     }
   )
 )
+
+
+
+Queue <- R6Class("Queue",
+                 public = list(
+                   initialize = function(...) {
+                     for (item in list(...)) {
+                       self$add(item)
+                     }
+                   },
+                   add = function(x) {
+                     private$queue <- c(private$queue, list(x))
+                     invisible(self)
+                   },
+                   remove = function() {
+                     if (private$length() == 0) return(NULL)
+                     # Can use private$queue for explicit access
+                     head <- private$queue[[1]]
+                     private$queue <- private$queue[-1]
+                     head
+                   }
+                 ),
+                 private = list(
+                   queue = list(),
+                   length = function() base::length(private$queue)
+                 )
+)
+
+q <- Queue$new(5, 6, "foo")
+
+
+Numbers <- R6Class("Numbers",
+                   public = list(
+                     x = 100
+                   ),
+                   active = list(
+                     x2 = function(value) {
+                       if (missing(value)) return(self$x * 2)
+                       else self$x <- value/2
+                     },
+                     rand = function() rnorm(1)
+                   )
+)
+
+n <- Numbers$new()
+n$x
+#> [1] 100
