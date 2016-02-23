d08edb7a19014af41f161823262e3c4b4830173d
chiu <chiucode@gmail.com>
2016-02-23 15:14:59 -0800

    Fixed keypress bug. with keypress not being changed; use random number!

---
+++ onInputChange/ui.R
@@ -0,0 +1,40 @@
+# ui.R
+
+shinyUI(bootstrapPage(
+  # a div named mydiv
+  tags$div(
+    id = "mydiv",
+    style = "width: 50px; height :50px;
+    left: 100px; top: 100px;
+    background-color: gray; position: absolute"
+  ),
+  
+  # a shiny element to display unformatted text
+  verbatimTextOutput("results"),
+  
+  # javascript code to send data to shiny server
+  tags$script(
+    '
+    $(document).on("keypress", function (e) {
+    Shiny.onInputChange("keypress_value", e.which);
+    var number = Math.random();
+    Shiny.onInputChange("random_number", number);
+    });
+    '
+  )#,
+  # tags$script(
+  #   '
+  #   $(document).on("keypress", function (e) {
+  #   var number = Math.random();
+  #   Shiny.onInputChange("random_number", number);
+  #   });
+  #   '
+  # )
+  # tags$script('
+  #             document.getElementById("mydiv").onclick = function() {
+  #             var number = Math.random();
+  #             Shiny.onInputChange("random_number", number);
+  #             };
+  #             ')
+  
+  ))
